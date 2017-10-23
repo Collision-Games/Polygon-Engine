a7223a51416ba3101590feb7bca757f5e47a9052
Daan Meijer <daanhmc@gmail.com>
2017-10-23 20:02:00 +0200

    Added basic buffers

--- src/com/collisiongames/polygon/buffers/Buffer.java
+++ src/com/collisiongames/polygon/buffers/Buffer.java
@@ -1,32 +1,62 @@
 package com.collisiongames.polygon.buffers;
 
-import org.lwjgl.opengl.GL11;
+import java.util.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
+/**
+ * Base class for all OpenGL buffers including but not limited to Vertex Arrays, Buffer Objects, RenderBuffers, Textures and RenderBuffers
+ *
+ * @author Daan Meijer
+ * @since v0.1
+ */
 public abstract class Buffer {
 
     private static Map<Integer, Integer> boundBuffers = new HashMap<>();
     protected int bufferID, bufferType;
 
-    public Buffer(int bufferID, int bufferType) {
+    /**
+     * Constructor for a instance of a {@link Buffer} class
+     *
+     * @param bufferID   the generated buffer id by OpenGL(glGenBuffers etc.)
+     * @param bufferType the type of buffer like: GL_VERTEX_ARRAY, GL_ELEMENT_ARRAY_BUFFER and GL_ARRAY_BUFFER
+     */
+    protected Buffer(int bufferID, int bufferType) {
         this.bufferID = bufferID;
         this.bufferType = bufferType;
     }
 
+    /**
+     * Function for deleting the {@link Buffer} instance from memory
+     */
     public final void delete() {
         onDelete();
     }
 
+    /**
+     * Binding the current buffer for OpenGL calls
+     */
     public final void bind() {
         onBind();
         boundBuffers.put(bufferType, bufferID);
     }
 
+    /**
+     * Clearing the currently bound buffer
+     */
     public final void unBind() {
         onUnBind();
         boundBuffers.put(bufferType, 0);
+    }
+
+    /**
+     * Checks if the specified buffer is currently bound for OpenGL calls
+     *
+     * @param bufferType the type of buffer to check, like: GL_VERTEX_ARRAY, GL_ELEMENT_ARRAY_BUFFER and GL_ARRAY_BUFFER
+     * @return true if the buffer is currently bound
+     * @see Buffer#bind()
+     */
+    public final boolean isBound(int bufferType) {
+        if (!boundBuffers.containsKey(bufferType)) return false;
+        return boundBuffers.get(bufferType) != null;
     }
 
     protected abstract void onDelete();
--- src/com/collisiongames/polygon/buffers/VertexArray.java
+++ src/com/collisiongames/polygon/buffers/VertexArray.java
@@ -1,12 +1,89 @@
 package com.collisiongames.polygon.buffers;
 
-import static org.lwjgl.opengl.GL11.GL_VERTEX_ARRAY;
+import org.lwjgl.opengl.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.*;
 import static org.lwjgl.opengl.GL30.*;
+import static org.lwjgl.opengl.GL33.*;
 
+/**
+ * A class for storing a Vertex Array buffer.
+ * Vertex Arrays as mostly used for storing vertex data
+ * and element data on it for later rendering.
+ *
+ * @since v0.1
+ * @author Daan Meijer
+ */
 public class VertexArray extends Buffer {
 
-    public VertexArray() {
+    //TODO check if data can be put into one buffer
+    protected BufferObject elementBuffer, vertexBuffer;
+
+    /**
+     * Constructor for a instance of {@link VertexArray}
+     *
+     * @param vertices the vertex data of a model
+     * @param indices the element(index data) of a model
+     */
+    public VertexArray(float[] vertices, int[] indices) {
         super(glGenVertexArrays(), GL_VERTEX_ARRAY);
+        bind();
+
+        elementBuffer = new BufferObject(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW, indices);
+        vertexBuffer = new BufferObject(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices);
+    }
+
+    /**
+     * Store an array of generic vertex attribute data
+     *
+     * @param index specifies the index of the buffer to modified, max range is 16
+     * @param size the numbers of components per generic vertex attribute, must be 1, 2, 3 or 4
+     * @param dataType the data type of each component in the array, can be GL_BYTE, GL_SHORT, GL_INT, GL_LONG etc.
+     * @param stride the byte offset between consecutive generic vertex attributes, If stride is 0, the generic vertex attributs are understood
+     *               to be tightly packed in the array
+     * @param offset specifies the byte offset of the first component of the first generic vertex attribute in the array in the data store
+     * @param bufferObject specifies the buffer to retrieve the data from
+     * @param normalized specifies if the fixed-point data should be normalized(true) when they are accessed
+     */
+    public void assignAttribPointer(int index, int size, int dataType, int stride, int offset, BufferObject bufferObject, boolean normalized) {
+        bind();
+        assert bufferObject != null;
+        bufferObject.bind();
+        glEnableVertexAttribArray(index);
+        glVertexAttribPointer(index, size, dataType, normalized, stride, offset);
+    }
+
+    /**
+     * Store an array of generic vertex attribute data
+     *
+     * @param index specifies the index of the buffer to modified, max range is 16
+     * @param size the numbers of components per generic vertex attribute, must be 1, 2, 3 or 4
+     * @param dataType the data type of each component in the array, can be GL_BYTE, GL_SHORT, GL_INT, GL_LONG etc.
+     * @param stride the byte offset between consecutive generic vertex attributes, If stride is 0, the generic vertex attributs are understood
+     *               to be tightly packed in the array
+     * @param offset specifies the byte offset of the first component of the first generic vertex attribute in the array in the data store
+     *               of the buffer currently bound to GL_ARRAY_BUFFER target
+     * @param normalized specifies if the fixed-point data should be normalized(true) when they are accessed
+     */
+    public void assignAttribPointer(int index, int size, int dataType, int stride, int offset, boolean normalized) {
+        bind();
+
+        assert isBound(GL_ARRAY_BUFFER);
+
+        glEnableVertexAttribArray(index);
+        glVertexAttribPointer(index, size, dataType, normalized, stride, offset);
+    }
+
+    /**
+     * Modify the rate at which generic vertex attributes advance during instanced rendering
+     *
+     * @param index specifies the index of the vertex attribute
+     * @param divisor specifies the number of instances the will pass between updates of the generic attribute at slot index
+     */
+    public void setAttribDivisor(int index, int divisor) {
+        bind();
+        glVertexAttribDivisor(index, divisor);
     }
 
     @Override
