b42aef1dcffecf698b401932b4df3ef440a7dd0b
Alex Ringhoffer <alex@howaboutdoge.com>
2017-10-23 21:15:02 +0100

    Added comments

--- src/com/collisiongames/polygon/graphics/Window.java
+++ src/com/collisiongames/polygon/graphics/Window.java
@@ -3,14 +3,23 @@ package com.collisiongames.polygon.graphics;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.opengl.*;
 
+/**
+ * 
+ * @author Alex Ringhoffer
+ *
+ */
 public class Window {
 	
 	private static final int
 		WIDTH = 1280,
 		HEIGHT = 720;
 	
+	// ID of the window
 	private long ID;
 	
+	/**
+	 * Default constructor
+	 */
 	public Window() {
 		
 		GLFW.glfwDefaultWindowHints();
@@ -24,17 +33,28 @@ public class Window {
 		
 		GLFW.glfwMakeContextCurrent(ID);
 		GLFW.glfwShowWindow(ID);
+		
 		GL.createCapabilities();
 		GL11.glViewport(0, 0, WIDTH, HEIGHT);
 	}
 	
+	/**
+	 * Swap buffers and run events;
+	 */
 	public void update() {
 		
 		GLFW.glfwPollEvents();
 		GLFW.glfwSwapBuffers(ID);
 	}
 	
+	/**
+	 * 
+	 * @return Should this window close now?
+	 */
 	public boolean shouldClose() { return GLFW.glfwWindowShouldClose(ID); }
 	
+	/**
+	 * Destroy the window
+	 */
 	public void dispose() { GLFW.glfwDestroyWindow(ID); }
 }
--- src/com/collisiongames/polygon/util/Client.java
+++ src/com/collisiongames/polygon/util/Client.java
@@ -2,6 +2,15 @@ package com.collisiongames.polygon.util;
 
 public class Client {
 
+	/**
+	 * 
+	 * @return Is this client a Mac?
+	 */
 	public static boolean isMac() { return System.getProperty("os.name").toLowerCase().contains("mac"); }
+	
+	/**
+	 * 
+	 * @return Is this client Windows?
+	 */
 	public static boolean isWindows() { return System.getProperty("os.name").toLowerCase().contains("win"); }
 }
